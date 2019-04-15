04b31afcef7468b2b3db09d69d17e11800cdcc19
Bety <A01732177@itesm.mx>
2019-04-15 17:09:46 -0500

    archivos java

---
+++ Main.java
@@ -0,0 +1,23 @@
+/* Autores:
+ * Lourdes Beatriz Cajica Maceda A01732177
+ * Jacqueline Ortiz Sanchez A01329978
+ *
+ * Descripcion:
+ * Simulacion del protocolo de capa 1 DNS.
+ * El Main es el browser que busca saber cual es el IP del dominio.
+ * Se pueden agregar mas sitios web.
+ *
+ * Ultima fecha de modificacion:
+ * 15-abril-19
+ *
+ * Falta:
+ */
+
+public class Main{
+  public static void main(String[] args) {
+    /* Cargar informacion */
+
+
+    /* Desplegar menu */
+  }
+}
---
+++ Servidor.java
@@ -0,0 +1,17 @@
+/* Autores:
+ * Lourdes Beatriz Cajica Maceda A01732177
+ * Jacqueline Ortiz Sanchez A01329978
+ *
+ * Descripcion:
+ * Objeto Servidor.
+ *
+ * Ultima fecha de modificacion:
+ * 15-abril-19
+ *
+ * Falta:
+ *
+ */
+public class Servidor{
+  String nombre;
+
+}

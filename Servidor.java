dafa81930706f68252579711015d2d556dcc79ba
Bety <A01732177@itesm.mx>
2019-04-17 12:53:43 -0500

    agregar variables y constructor a la clase servidor

--- Servidor.java
+++ Servidor.java
@@ -6,13 +6,23 @@
  * Objeto Servidor.
  *
  * Ultima fecha de modificacion:
- * 15-abril-19
+ * 17-abril-19
  *
  * Falta:
- * 
+ *
  */
 public class Servidor{
   /* variables */
-
+  String dominio;
+  String tld;
+  String ip;
+  
   /* metodos */
+
+  /* constructor */
+  public Servidor(String dominio, String tld, String ip){
+    this.dominio = dominio;
+    this.tld = tld;
+    this.ip = ip;
+  }
 }

cbe81bbb9858e0fd6508072cd815800416b70677
Bety <A01732177@itesm.mx>
2019-04-25 16:49:16 -0500

    agrega metodo encontrar ip

--- Resolver.java
+++ Resolver.java
@@ -12,14 +12,31 @@
  *
  */
 
+import java.util.ArrayList;
+
 public class Resolver{
   /* variables */
   ArrayList<String> sitio;
   ArrayList<String> ip;
+
   /* metodos */
   /* constructor */
   public Resolver(){
     sitio = new ArrayList<String>();
     ip = new ArrayList<String>();
+  }
+
+  /* inicia la busqueda del ip */
+  public String encontrarIp(String pagina){
+    /* empieza en su memoria */
+    for(int i = 0; i < sitio.size(); i++){
+      if(sitio.get(i) == pagina){
+        return ip.get(i);
+      }
+    }
+
+    /* llama al root */
+
+    return "Algo salio mal";
   }
 }

284ca3c8755f4b80ec2aec5f5147a5f2fa361fd8
Bety <A01732177@itesm.mx>
2019-04-25 21:15:15 -0500

    modificar comentarios

--- Main.java
+++ Main.java
@@ -6,47 +6,18 @@
  * Simulacion del protocolo de capa 1 DNS.
  * El Main es el browser que busca saber cual es el IP del dominio.
  * Se pueden agregar mas sitios web.
- *
- * Ultima fecha de modificacion:
- * 17-abril-19
- *
- * Falta:
- * -> cargar informacion de un txt
- * -> guardar la informacion de un txt
  */
 import java.util.Scanner;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.BufferedReader;
 
 public class Main{
   public static void main(String[] args) {
-    /* variables menu */
+    /* variables */
     Scanner scan = new Scanner(System.in);
     int opcion = 9999;
-    /* variables lectura de archivos */
-    String file = "data.txt";
-    String linea = "";
-    int n = 0;
-    /* variables servidores */
 
     /* cargar informacion */
-    try{
-      FileReader fl = new FileReader(file);
-      BufferedReader br = new BufferedReader(fl);
 
-      /* cargar informacion al resolver */
-      n = Integer.parseInt(BufferedReader.readLine());
 
-      /* cargar informacion al root */
-      n = Integer.parseInt(BufferedReader.readLine());
-      
-      /* cargar informacion a los tld */
-
-    }
-    catch(IOException e){
-      System.out.println("Ocurrio un error al leer el archivo");
-    }
     /* desplegar menu */
     do{
       System.out.println("Elige una opcion:\n1. Dar de alta un dominio\n2. Buscar la ip de un dominio\n0. Salir");
@@ -64,5 +35,6 @@ public class Main{
     }while(opcion != 0);
 
     /* guardar informacion */
+    gurdarInfo();
   }
 }

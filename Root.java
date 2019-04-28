51fbe44af61e0e0a6059aa3f8194d78544a168b8
jaorsa <38358814+jaorsa@users.noreply.github.com>
2019-04-27 22:40:27 -0500

    Revisar metodo setTld

--- Root.java
+++ Root.java
@@ -20,10 +20,15 @@ public class Root{
   /* set tld */
   public void setTld(Tld nuevo, String nombre){
     /* terminar */
+    Tld newTld = new Tld(nombre);
   }
 
   /* get tld */
   public Tld getT(String nombre){
     /* terminar */
+    for(int i = 0; i < tlds.size(); i++){
+      if(tlds.get(i).getTld() == nombre){
+        return tlds.get(i);
+      }
   }
 }

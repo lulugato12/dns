742a22a7f8eeb322cad9dbce08211b5b6e372330
Bety <A01732177@itesm.mx>
2019-04-29 22:44:55 -0500

    mensajes al 100 fierro pariente

--- README.md
+++ README.md
@@ -4,11 +4,3 @@ Proyecto final de Fundamentos de Redes. Simulación del protocolo DNS.
 Colaboradores:
 @lulugato12
 @jaorsa
-
-**Falta por resolver:**
-- [x] agregar tld en main
-- [x] objeto Root: hacer funciones
-- [ ] metodo para buscar ip |trabaja: @jaorsa
-- [x] metodo agregar servidor
-- [ ] hacer metodo para imprimir mensaje DNS en cada clase |trabaja: @lulugato12
-- [x] agregar ip a todas las clases
--- Root.java
+++ Root.java
@@ -9,11 +9,13 @@ import java.util.ArrayList;
 
 public class Root{
   /* variables */
+  private String ip;
   private ArrayList<Tld> tlds;
 
   /* metodos */
   /* constructor */
-  public Root(){
+  public Root(String ip){
+    this.ip = ip;
     tlds = new ArrayList<Tld>();
   }
 
@@ -21,7 +23,7 @@ public class Root{
   public void setTld(Tld nuevo, String nombre){
     boolean agregado = false;
     for(int i = 0; i < tlds.size(); i++){
-      if(tlds.get(i).getTld() == nombre){
+      if(tlds.get(i).getTld().equals(nombre)){
         tlds.set(i, nuevo);
         agregado = true;
       }
@@ -31,14 +33,17 @@ public class Root{
     }
   }
 
-  /* get tld */
+  /* gets*/
   public Tld getT(String nombre){
     for(int i = 0; i < tlds.size(); i++){
-      System.out.println(tlds.get(i).getTld());
       if(tlds.get(i).getTld().equals(nombre)){
         return tlds.get(i);
       }
     }
     return null;
+  }
+
+  public String getIp(){
+    return ip;
   }
 }
